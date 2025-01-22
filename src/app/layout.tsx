d570bf26d86a9a5036164c152e7fe07a3e78a348
Angad Anil Gosain <angadgosain@gmail.com>
2025-01-22 13:18:53 -0800

    Created the Create project component, created Project model in Prisma, created backend functionality to store the created project.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,8 @@ import { GeistSans } from "geist/font/sans";
 import { type Metadata } from "next";
 import {ClerkProvider} from '@clerk/nextjs'
 import { TRPCReactProvider } from "@/trpc/react";
+import { Toaster } from "sonner";
+
 
 export const metadata: Metadata = {
   title: "DevSage",
@@ -19,6 +21,7 @@ export default function RootLayout({
       <html lang="en" className={`${GeistSans.variable}`}>
       <body>
         <TRPCReactProvider>{children}</TRPCReactProvider>
+        <Toaster richColors />
       </body>
     </html>
     </ClerkProvider>
