8248fe6fbcdd50fa52048894cc2017a76109e105
Angad Anil Gosain <angadgosain@gmail.com>
2025-01-25 19:13:37 -0800

    Added a Dialog popup when clicking submit button.

--- src/app/(protected)/dashboard/ask-question-card.tsx
+++ src/app/(protected)/dashboard/ask-question-card.tsx
@@ -1,28 +1,42 @@
 'use client'
 import { Button } from '@/components/ui/button'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import { Dialog, DialogContent, DialogHeader, DialogTitle } from '@/components/ui/dialog'
 import { Textarea } from '@/components/ui/textarea'
 import useProject from '@/hooks/use-project'
+import Image from 'next/image'
 import React, { useState } from 'react'
 
 const AskQuestionCard = () => {
     const {project} = useProject()
     const [question, setQuestion] = useState('')
+    const [open, setOpen] = useState(false)
+    console.log('Dialog open state:', open);
 
     const onSubmit = async(e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault()
-        window.alert(question)
+        setOpen(true)
     }
 
   return (
     <>
-        <Card className='relative col-span-2'>
+        <Dialog open={open} onOpenChange={setOpen}>
+            <DialogContent>
+                <DialogHeader>
+                    <DialogTitle>
+                        <Image src='/logo.png' alt='devsage' width={80} height={80} />
+                    </DialogTitle>
+                </DialogHeader>
+            </DialogContent>
+        </Dialog>
+
+        <Card className='relative col-span-3'>
             <CardHeader>
                 <CardTitle>Ask a question</CardTitle>
             </CardHeader>
             <CardContent>
                 <form onSubmit={onSubmit}>
-                    <Textarea placeholder='Which file should I edit to change the home page?' />
+                    <Textarea placeholder='Which file should I edit to change the home page?' value={question} onChange={e => setQuestion(e.target.value)} />
                     <div className='h-4'></div>
                     <Button type='submit'>
                         Ask DevSage!
